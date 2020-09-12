c4e79d2c80426899f9768a53d770fcf462af00cd
Diwash Rai <diwash.rai1993@gmail.com>
2020-09-12 19:41:06 +0100

    sorting visualisation looks a bit better. Need to create controls

--- js/sorting-algorithm-visualiser.js
+++ js/sorting-algorithm-visualiser.js
@@ -1,8 +1,10 @@
-const total_bars = 60;
+const total_bars = 400;
+let bar_color = "white";
+let select_color = "red";
 
-var arr = [];
+let arr = [];
 for (i = 0; i < total_bars; ++i) {
-    arr.push(Math.ceil(Math.random() * 736));
+    arr.push(Math.ceil(Math.random() * 600));
 }
 
 console.log(arr);
@@ -14,7 +16,7 @@ let bars_arr = Array(total_bars).fill(0);
 for (i = 0; i < total_bars; ++i) {
     bars_arr[i] = document.createElement("div");
     bars_arr[i].classList.add("bar");
-    bars_arr[i].style.width = arr[i] + "px";
+    bars_arr[i].style.height = arr[i] + "px";
     area.appendChild(bars_arr[i]);
 }
 
@@ -31,13 +33,13 @@ async function partition(arr, left, right) {
             --right;
         [arr[left], arr[right]] = [arr[right], arr[left]];
 
-        bars_arr[left].style.backgroundColor = "purple";
-        bars_arr[right].style.backgroundColor = "purple";
-        await sleep(200);
-        bars_arr[left].style.width = arr[left] + "px";
-        bars_arr[right].style.width = arr[right] + "px";
-        bars_arr[left].style.backgroundColor = "green";
-        bars_arr[right].style.backgroundColor = "green";
+        bars_arr[left].style.backgroundColor = select_color;
+        bars_arr[right].style.backgroundColor = select_color;
+        await sleep(10);
+        bars_arr[left].style.height = arr[left] + "px";
+        bars_arr[right].style.height = arr[right] + "px";
+        bars_arr[left].style.backgroundColor = bar_color;
+        bars_arr[right].style.backgroundColor = bar_color;
     }
     return left;
 }
@@ -62,13 +64,13 @@ async function bubble_sort(arr) {
                 arr[j] = arr[j + 1];
                 arr[j + 1] = store;
             }
-            bars_arr[j].style.backgroundColor = "purple";
-            bars_arr[j + 1].style.backgroundColor = "purple";
-            bars_arr[j].style.width = arr[j] + "px";
-            bars_arr[j + 1].style.width = arr[j + 1] + "px";
+            bars_arr[j].style.backgroundColor = select_color;
+            bars_arr[j + 1].style.backgroundColor = select_color;
+            bars_arr[j].style.height = arr[j] + "px";
+            bars_arr[j + 1].style.height = arr[j + 1] + "px";
             await sleep(10);
-            bars_arr[j].style.backgroundColor = "green";
-            bars_arr[j + 1].style.backgroundColor = "green";
+            bars_arr[j].style.backgroundColor = bar_color;
+            bars_arr[j + 1].style.backgroundColor = bar_color;
         }
         if (sorted) {
             break;
